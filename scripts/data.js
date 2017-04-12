1ec64c612e1a2d6cfe00cf2ff71080bb0c44c32d
Bret Ldenburg <bret.ladenburg@gmail.com>
2017-04-11 17:48:58 -0700

    made changes to my portfolio that will help my partner programming tomorrow

---
+++ scripts/data.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var rawData = [
+  {
+    title:       'Four In A Row',
+    category:    'game',
+    authors:      ['James Feore', 'Bret Ladenburg', 'Timothy Tran', 'Max Johnson'],
+    gitHubUrl:   '',
+    publishedOn: 'March 31, 2017',
+    body:        'This is a neat four in a row project that I did when I was in 201!'
+  }
+]
